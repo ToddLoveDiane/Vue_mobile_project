04bed5915343d8564b0ae80aaa2bfa27ee66a482
ToddLoveDianee <627784964@qqe.com>
2019-09-14 20:28:00 +0800

    02-完成登录页面的基本布局

--- vue_mobile_project/src/main.js
+++ vue_mobile_project/src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//使用vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+//使用validate
+import VeeValidate from 'vee-validate'
+Vue.use(VeeValidate)
 
 Vue.config.productionTip = false
 
---
+++ vue_mobile_project/src/views/login/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="mylogin">
+    <!-- 头部 -->
+    <van-nav-bar class="myhead" />
+    <!-- 输入框 -->
+    <van-cell-group>
+      <van-field
+        v-model="mobile"
+        left-icon="contact"
+        clearable
+        required
+        label="用户名"
+        placeholder="请输入正确的手机号"
+      />
+      <van-field
+        v-model="code"
+        left-icon="eye-o"
+        type="text"
+        label="密码"
+        placeholder="请输入验证码"
+        required
+      />
+    </van-cell-group>
+    <!-- 按钮 -->
+    <div class="mybtn">
+      <van-button type="info" size="large">登录</van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mobile: "",
+      code: ""
+    };
+  }
+};
+</script>
+
+<style lang='less' scoped>
+.myhead {
+  background-color: #3296fa;
+}
+.mybtn {
+  margin: 20px;
+  .van-button--large {
+    border-radius: 8px;
+  }
+}
+</style>
