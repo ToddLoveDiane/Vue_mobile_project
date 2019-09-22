d19bf32f339ea164432d36ea24d187682c675800
ToddLoveDianee <627784964@qqe.com>
2019-09-22 14:10:45 +0800

    13_2 完成用户喜欢,点赞功能

--- vue_mobile_project/src/api/userInfo.js
+++ vue_mobile_project/src/api/userInfo.js
@@ -29,13 +29,49 @@ function noFollow(autId) {
     return request({
         url: `v1_0/user/followings/${autId}`,
         method: 'DELETE',
-
     })
 }
-
+//点赞
+function thumbsUp(artId) {
+    return request({
+        url: 'v1_0/article/likings',
+        method: 'POST',
+        data: {
+            target: artId
+        }
+    })
+}
+//取消点赞
+function thumbsDown(artId) {
+    return request({
+        url: `v1_0/article/likings/${artId}`,
+        method: 'DELETE',
+    })
+}
+//喜欢文章
+function iLike(artId){
+    return request({
+        url: `v1_0/article/dislikes/${artId}`,
+        method: 'DELETE',
+    })
+}
+//不喜欢
+function iHate(artId){
+    return request({
+        url: 'v1_0/article/dislikes',
+        method: 'POST',
+        data: {
+            target: artId
+        }
+    })
+}
 //3.暴露接口 如果没有写default就用{}对象
 export {
     userLogin,
     getFollow,
-    noFollow
+    noFollow,
+    thumbsDown,
+    thumbsUp,
+    iLike,
+    iHate
 }
